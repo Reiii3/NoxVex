8792496a76b702f96fc49fb6689424099efb0bd6
Reiii3 <mohtoyib79@gmail.com>
2025-07-06 08:45:55 +0700

    update file-ai/aiSystem.sh

--- file-ai/aiSystem.sh
+++ file-ai/aiSystem.sh
@@ -1,10 +1,66 @@
 IDLE_TIME=5
 gamerun=""
 notif_run=""
+time=$(date "+%d-%m-%Y %H:%M")
 menu1=$(settings get global menu_gvr_smart_noxg1)
 menu2=$(settings get global menu_gvr_smart_noxg2)
 menu3=$(settings get global menu_gvr_smart_noxg3)
 menu4=$(settings get global menu_gvr_smart_noxg4)
+
+smartSystemRun() {
+  echo
+  echo "[start system-running]"
+  if [[ "$menu1" == "true" ]]; then
+    echo " [$time] Fitur 1 is actived smart system"
+  else
+    echo " [$time] Fitur 1 is not-actived"
+  fi
+  if [[ "$menu2" == "true" ]]; then
+    echo " [$time] Fitur 2 is actived smart system"
+  else
+    echo " [$time] Fitur 2 is not-actived"
+  fi
+  if [[ "$menu3" == "true" ]]; then
+    echo " [$time] Fitur 3 is actived smart system"
+  else
+    echo " [$time] Fitur 3 is not-actived"
+  fi
+  if [[ "$menu4" == "true" ]]; then
+    echo " [$time] Fitur 4 is actived smart system"
+  else
+    echo " [$time] Fitur 4 is not-actived"
+  fi
+  echo "[end system-running]"
+  echo
+}
+
+smartSystemRmv() {
+  echo
+  echo "[start system-remove]"
+  if [[ "$menu1" == "true" ]]; then
+    echo " [$time] Fitur 1 is actived smart system"
+  else
+    echo " [$time] Fitur 1 is not-actived"
+  fi
+  if [[ "$menu2" == "true" ]]; then
+    echo " [$time] Fitur 2 is actived smart system"
+  else
+    echo " [$time] Fitur 2 is not-actived"
+  fi
+  if [[ "$menu3" == "true" ]]; then
+    echo " [$time] Fitur 3 is actived smart system"
+  else
+    echo " [$time] Fitur 3 is not-actived"
+  fi
+  if [[ "$menu4" == "true" ]]; then
+    echo " [$time] Fitur 4 is actived smart system"
+  else
+    echo " [$time] Fitur 4 is not-actived"
+  fi
+  echo "[end system-remove]"
+  echo
+}
+
 test_logic() {
    game=$(settings get global package_gvr_noxg)
    detected_apps=$(dumpsys window | grep "Window #" | grep WindowStateAnimator | grep -v "Window #0" | grep -Eo "$game")
@@ -13,7 +69,7 @@ test_logic() {
         if [[ "$gamerun" != "running" ]] && [[ "$render_detected" != "skiavk" ]]; then
            if [[ "$notif_run" != "run" ]]; then
               cmd notification post -S bigtext -t "Smart Syatem" "nox_ai_status" "Mode Game : ON"
-              echo "[DEBUG] : running notif"
+              smartSystemRun
               notif_run="run"
            fi
           gamerun="running"
@@ -23,6 +79,7 @@ test_logic() {
            if [[ "$notif_run" != "stop" ]]; then
               cmd notification post -S bigtext -t "Smart Syatem" "nox_ai_status" "Mode Game : OFF"
               notif_run="stop"
+              smartSystemRmv
            fi
           gamerun="stopped"
         fi
