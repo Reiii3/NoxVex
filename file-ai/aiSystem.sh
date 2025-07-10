2b1cc02dd150406f5e3a4d1c8fb6d9a483769708
Reiii3 <mohtoyib79@gmail.com>
2025-07-11 01:13:40 +0700

    update file-ai/aiSystem.sh

--- file-ai/aiSystem.sh
+++ file-ai/aiSystem.sh
@@ -73,11 +73,45 @@ smartSystemRun() {
   fi
   if [[ "$menu2" == "true" ]]; then
     echo " [$time] Fitur 2 is actived smart system"
+    settings delete global updatable_driver_production_opt_in  >/dev/null 2>&1;
+    settings put global updatable_driver_production_opt_in_apps $detected_apps;
   else
     echo " [$time] Fitur 2 is not-actived"
   fi
   if [[ "$menu3" == "true" ]]; then
     echo " [$time] Fitur 3 is actived smart system"
+    # source Code By kazuyo
+    setprop debug.composition.type mdp
+    setprop debug.hwc.dynThreshold 4.5
+    setprop debug.enable.sglscale 1
+    setprop debug.hwui.disable_vsync true
+    setprop debug.hwui.target_gpu_time_percent 100
+    setprop debug.gpuprio 7
+    setprop debug.sf.gpu_freq_index 7
+    setprop debug.sf.cpu_freq_index 7
+    setprop debug.ioprio 7
+    setprop debug.sf.mem_freq_index 7
+    if [[ "$soc" == "Mediatek" ]]; then
+     setprop debug.mediatek.appgamepq_compress 1
+     setprop debug.mediatek.disp_decompress 1
+     setprop debug.mediatek.appgamepq 2
+     setprop debug.mediatek.game_pq_enable 1
+     setprop debug.mediatek.high_frame_rate_sf_set_big_core_fps_threshold 119
+     setprop debug.mtklog.netlog.enable 0
+     setprop debug.mtklog.aee.Running 0
+     setprop debug.mtklog.aee.enable 0
+     setprop debug.mtklog.log2sd.path ""
+     setprop debug.mtk.aee.db 0
+     setprop debug.MB.running 0
+   elif [[ "$soc" == "Qualcom" ]]; then
+     setprop debug.qti.am.resource.type super-large
+     setprop debug.qc.hardware true
+     setprop debug.qctwa.preservebuf 1
+     setprop debug.qualcomm.sns.libsensor1 0
+     setprop debug.qualcomm.sns.daemon 0
+     setprop debug.qualcomm.sns.hal 0
+     setprop debug.qctwa.statusbar 1
+   fi
   else
     echo " [$time] Fitur 3 is not-actived"
   fi
