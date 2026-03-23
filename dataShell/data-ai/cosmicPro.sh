e4725b1b497a938904c00f820e382cb41e4cda87
Jaa <mohtoyib79@gmail.com>
2026-03-24 04:30:02 +0700

    update dataShell/data-ai/cosmicPro.sh

--- dataShell/data-ai/cosmicPro.sh
+++ dataShell/data-ai/cosmicPro.sh
@@ -308,7 +308,7 @@ service_engine() {
     running_mode_detection=""
     notif_state="run"
 
-    settings put global cosmic_engine_version 1.0.3_BETA
+    settings put global cosmic_engine_version 1.0.4_BETA
     settings put global cosmic_engine_enable cosmicp_server.pid
     
     echo "[Service] COSMIC Pro Started at $(date)" >> "$LOG_FILE"
