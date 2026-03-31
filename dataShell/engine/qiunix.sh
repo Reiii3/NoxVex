3c7863b09f2a6ef05cd807f7eb655d6cc88597b0
Reiii3 <rizamaulanaa3@gmail.com>
2026-03-31 21:40:31 +0700

    penambahan engine qiunix for instalasi engine ai

---
+++ dataShell/engine/qiunix.sh
@@ -0,0 +1,58 @@
+engineStart() {
+    chmod 777 /data/user_de/0/com.android.shell/axeron/xbin/qiunixAI
+    if [[ -f /data/local/tmp/qiunixai.pid ]]; then
+        PID=$(cat /data/local/tmp/qiunixai.pid)
+        if kill -0 $PID > /dev/null 2>&1; then
+            echo "QiunixAI sedang berjalan"
+            return
+        else 
+            rm -f /data/local/tmp/qiunixai.pid
+            rm -f /data/local/tmp/qiunixai.log
+        fi
+    fi
+
+    nohup /data/user_de/0/com.android.shell/axeron/xbin/qiunixAI > /data/local/tmp/qiunixai.log 2>&1 &
+    # Menyimpan PID ke file
+    echo $! > /data/local/tmp/qiunixai.pid
+
+    if [[ -f /data/local/tmp/qiunixai.pid ]]; then
+        echo "QiunixAI berhasil diaktifkan"
+    else
+        echo "QiunixAI gagal diaktifkan"
+    fi
+}
+
+engineStop() {
+    if [[ ! -f /data/local/tmp/qiunixai.pid ]]; then
+        echo "QiunixAI sedang tidak aktif"
+        exit 0
+    fi
+
+    PID=$(cat /data/local/tmp/qiunixai.pid)
+    if ! kill -0 $PID > /dev/null 2>&1; then
+        rm -f /data/local/tmp/qiunixai.pid
+        rm -f /data/local/tmp/qiunixai.log
+        echo "QiunixAI sedang tidak aktif"
+        exit 0
+    fi
+
+    kill $PID
+    rm -f /data/local/tmp/qiunixai.pid
+    rm -f /data/local/tmp/qiunixai.log
+    echo "QiunixAI berhasil dinonaktifkan"
+}
+
+engineStatus() {
+    if [[ -f /data/local/tmp/qiunixai.pid ]]; then
+        PID=$(cat /data/local/tmp/qiunixai.pid)
+        if kill -0 $PID > /dev/null 2>&1; then
+            echo "RUNNING PID: $PID"
+        else
+            rm -f /data/local/tmp/qiunixai.pid
+            rm -f /data/local/tmp/qiunixai.log
+            echo "STOPPED"
+        fi
+    else
+        echo "STOPPED"
+    fi
+}
